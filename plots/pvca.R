58d9c8b7c08c4e844a44374e9d4ed532da331efd
Chentong Sun <111181142+gilmore307@users.noreply.github.com>
2026-01-18 02:48:05 +0800

    Reorder stacked components

--- plots/pvca.R
+++ plots/pvca.R
@@ -310,7 +310,7 @@ pvca_df <- dplyr::bind_rows(pvca_list) %>%
   dplyr::mutate(Method = factor(Method, levels = names(file_list)))
 
 # --------- Plot PVCA in pRDA style: no on-bar labels, table below ----------
-component_order <- c("Target","Intersection","Batch","Residuals")
+component_order <- c("Residuals","Batch","Intersection","Target")
 pvca_plot_df <- pvca_df %>%
   dplyr::mutate(
     Method    = factor(Method, levels = names(file_list)),
@@ -331,10 +331,11 @@ cols <- c(
 )
 
 p <- ggplot(pvca_plot_df, aes(x = Method, y = Fraction, fill = Component)) +
-  geom_col(width = 0.72, color = "white", linewidth = 0.4) +
+  geom_col(width = 0.72, color = "white", linewidth = 0.4,
+           position = position_stack(reverse = TRUE)) +
   scale_fill_manual(
     values = cols,
-    breaks = component_order,   # c("Target","Intersection","Batch","Residuals")
+    breaks = rev(component_order),   # c("Target","Intersection","Batch","Residuals")
     limits = component_order,
     name   = "Variance Components"
   )+
