fe5922098ac9b22e0e007a4eaf8329110c291a52
Chentong Sun <sunchentong.99@qq.com>
2026-06-14 05:36:37 -0400

    Protect target design in limma correction

--- correction/methods/limma.R
+++ correction/methods/limma.R
@@ -5,10 +5,28 @@ prepare_method("limma")
 run_method("limma", {
   require(limma)
   X_log <- get_input_for("limma", base_M, base_form)
+
+  limma_design <- matrix(1, nrow = nrow(metadata), ncol = 1)
+  if (TARGET_BINARY_COL %in% colnames(metadata)) {
+    target_vals <- suppressWarnings(as.numeric(metadata[[TARGET_BINARY_COL]]))
+    if (all(is.finite(target_vals)) && length(unique(target_vals)) > 1) {
+      limma_design <- model.matrix(~ target_vals)
+    }
+  }
+
+  limma_covariates <- covar
+  target_columns <- unique(c(label_col, make.names(label_col), TARGET_BINARY_COL, make.names(TARGET_BINARY_COL)))
+  target_columns <- target_columns[!is.na(target_columns)]
+  drop_target_cols <- intersect(colnames(limma_covariates), target_columns)
+  if (length(drop_target_cols)) {
+    limma_covariates <- limma_covariates[, !(colnames(limma_covariates) %in% drop_target_cols), drop = FALSE]
+  }
+
   adj_t <- removeBatchEffect(
     t(X_log),
     batch = factor(batch),
-    covariates = if (ncol(covar) > 0) as.matrix(covar) else NULL
+    covariates = if (ncol(limma_covariates) > 0) as.matrix(limma_covariates) else NULL,
+    design = limma_design
   )
   adj <- t(adj_t)
   write_tss_clr("limma", adj, "log", "normalized_limma.csv")
